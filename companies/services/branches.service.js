bb221a595b5240006f745d3ffce0c4bca296f41c
tikimcrzx <tikimioo723@gmail.com>
2020-04-07 13:14:09 -0500

    update respon

--- companies/services/branches.service.js
+++ companies/services/branches.service.js
@@ -159,7 +159,7 @@ let BranchesService = class BranchesService {
         this.messageBots(
           "Ordenar",
           branch.menu[index].name,
-          `${branch.menu[index].details[0].size}: $${branch.menu[index].details[0].price} ${branch.menu[index].details[1].size}: ${branch.menu[index].details[1].price} ${branch.menu[index].details[2].size}: ${branch.menu[index].details[2].price}`,
+          `${branch.menu[index].details[0].size}: $${branch.menu[index].details[0].price} ${branch.menu[index].details[1].size}: $${branch.menu[index].details[1].price} ${branch.menu[index].details[2].size}: $${branch.menu[index].details[2].price}`,
           branch.menu[index].image
         )
       );
