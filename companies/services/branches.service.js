6f5b15662d2e46a3b23e243d5554e2ff09dd6092
tikimcrzx <tikimioo723@gmail.com>
2020-04-14 13:42:47 -0500

    update message bot

--- companies/services/branches.service.js
+++ companies/services/branches.service.js
@@ -272,7 +272,7 @@ let BranchesService = class BranchesService {
         buttons: [
           {
             text: message,
-            postback: "https://animeflv.net/",
+            postback: "OrdenarPrueba",
           },
         ],
       },
